3a924ddb3e3721b969e7db0a3b3df6504f9daf4a
Nick Sharp <nick.sharp.87@gmail.com>
2022-04-12 10:22:35 +0100

    feat: adding s3 backend and aws version lock

---
+++ environment/version.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "sharpn-terraform-state" // change this to your bucket name
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+
+    # dynamodb_table = "sharpn_terraform_lock" // change this to your dynamodb table name
+    # encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+  }
+
+  required_version = ">=1.0.11"
+}
