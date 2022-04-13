ca14eae8506db24919044bf97eb2d6909771e290
Nick Sharp <nick.sharp.87@gmail.com>
2022-04-13 16:57:12 +0100

    feat: adding aws and kubernetes providers to fix the region

--- environment/providers.tf
+++ environment/providers.tf
@@ -1,15 +1,22 @@
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks_cluster.cluster_id
-# }
+data "aws_eks_cluster" "cluster" {
+  name = module.eks_cluster.cluster_id
+}
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks_cluster.cluster_id
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks_cluster.cluster_id
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   load_config_file       = false
-#   version                = "~> 1.11"
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
